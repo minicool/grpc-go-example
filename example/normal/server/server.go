ac678025749ff64535b586502bb420a56ced6576
minicool <minicool.software@gamil.com>
2017-09-01 15:35:40 +0800

    add server path

--- example/normal/server/server.go
+++ example/normal/server/server.go
@@ -20,6 +20,19 @@ import (
 	"strings"
 )
 
+func substr(s string, pos, length int) string {
+	runes := []rune(s)
+	l := pos + length
+	if l > len(runes) {
+		l = len(runes)
+	}
+	return string(runes[pos:l])
+}
+
+func getParentDirectory(dirctory string) string {
+	return substr(dirctory, 0, strings.LastIndex(dirctory, "/"))
+}
+
 func getCurrentDirectory() string {
 	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
 	if err != nil {
